827d7c10d75ceba315559aad266c17e903ddb1b7
galihadityam1 <galihadityam1@gmail.com>
2024-03-28 23:33:00 +0700

    add register

--- client/App.js
+++ client/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
+import Register from "./screens/Register";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
--- client/screens/Login.js
+++ client/screens/Login.js
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import { Input } from "react-native-elements";
 import { Button } from "@rneui/themed";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Login = () => {
   return (
@@ -11,21 +11,27 @@ const Login = () => {
           <View className="relative flex flex-wrap">
             <View className="w-full relative">
               <View className="mt-6">
+                <View className="flex justify-center items-center h-12 mr-8">
+                  <Icon name="paw" size={30} color={"blue"} />
+                </View>
                 <View className="text-center items-center gap-4 flex">
                   <Text className="font-semibold text-black text-2xl px-10 w-80">
                     Welcome to Twittor
                   </Text>
-                  <View className="gap-4 w-screen mr-8 px-10">
+                  <View className="gap-4 w-screen mr-8 px-10 h-80 justify-center">
                     <Input
                       label="Sign In First"
-                      leftIcon={<Icon name="account-outline" size={20}/>}
-                      placeholder="Enter Name"
+                      leftIcon={<Icon name="account-outline" size={20} />}
+                      placeholder="Enter Username"
                     />
                     <Input
-                      leftIcon={<Icon name="key" size={20}/>}
+                      leftIcon={<Icon name="key" size={20} />}
                       placeholder="Enter Password"
                     />
                     <Button title={"Submit"} />
+                  </View>
+                  <View className="w-80 items-center justify-center flex h-6 mr-8">
+                    <Text>Don't You Have an Account? <Text className="text-blue-700 hover:underline">Sign Up</Text></Text>
                   </View>
                 </View>
               </View>
