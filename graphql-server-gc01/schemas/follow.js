3dfe0a37359c22d074135d081237ed75030b3cce
galihadityam1 <galihadityam1@gmail.com>
2024-04-01 04:10:11 +0700

    add in follow graphQL import

--- graphql-server-gc01/schemas/follow.js
+++ graphql-server-gc01/schemas/follow.js
@@ -1,5 +1,7 @@
 const { ObjectId } = require("mongodb");
 const User = require("../model/userModel");
+const { GraphQLError } = require("graphql");
+
 
 const typeDefsFollow = `#graphql
     type Follow {
